2cbb3e68ed1bc6f3551b80867a2cafd5ad0772f7
Ashley Archibald <aarchibald20@gmail.com>
2019-02-14 22:38:37 +0000

    Automatically backed up by Learn

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -103,7 +103,7 @@ describe "#using_insert" do
   before(:each) do 
     @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
     @another_esoteric_language = "Malbolge"
-    @new_array = using_insert(@list_of_esoteric_programming_languages, @another_esoteric_language)
+    another_esoteric_language = using_insert(@list_of_esoteric_programming_languages, @another_esoteric_language)
   end
 
   it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
